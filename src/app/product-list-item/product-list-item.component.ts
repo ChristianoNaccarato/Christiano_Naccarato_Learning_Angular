0ace9c5e3b4c0f6d742e0dea0181113b405a687e
Christiano <CN52@myscc.ca>
2025-10-03 08:48:04 -0400

    updated the product list item so HTML as it said in task 10

--- src/app/product-list-item/product-list-item.component.ts
+++ src/app/product-list-item/product-list-item.component.ts
@@ -1,12 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import { OnlineStore} from '../Shared/store';
+import {NgIf} from "@angular/common";
+import {Products} from "../product-list/product-list.component"
 
 @Component({
   selector: 'app-product-list-item',
   imports: [
     NgForOf,
-    NgForOf,],
+    NgForOf,
+    NgIf,
+    ],
   templateUrl: './product-list-item.component.html',
   styleUrl: './product-list-item.component.css'
 })
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,7 +3,7 @@ import {NgForOf} from '@angular/common';
 import {ProductListItemComponent} from '../product-list-item/product-list-item.component';
 import {OnlineStore} from '../Shared/store';
 // New interface for products
-interface Product {
+export interface Products {
   Product: string;
   Store: string;
   ProductID: number;
@@ -20,16 +20,16 @@ interface Product {
 })
 export class ProductListComponent {
   // Product list
-  productList:Product[] =[
+  productList:Products[] =[
     { Product: 'Apple', Store: 'Wallmart', ProductID: 1213, Price: 1 },
     { Product: 'Tire', Store: 'American Tire', ProductID: 6167, Price: 50 },
     { Product: 'Dog food', Store: 'Pet Food', ProductID: 6515, Price: 35 },
     { Product: 'T-Shirt', Store: 'Best Clothing', ProductID: 4415, Price: 20 }
   ];
   //Catch the onclick event from the html
-  selectedProduct?: Product;
+  selectedProduct?: Products;
   //function to set which product to display
-  selectProduct(product: Product): void {
+  selectProduct(product: Products): void {
     this.selectedProduct = product;
   }
 
