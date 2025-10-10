e9d3516484689f77f8997b4145f90b127dd5b3f9
Christiano <CN52@myscc.ca>
2025-10-09 22:03:13 -0400

    Move my content array out of contentList component and into its own file also created a service

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,9 @@
+import { Products } from '../product-list/product-list.component';
+
+export const PRODUCTLIST: Products[] = [
+  { Product: 'Apple', Store: 'Wallmart', ProductID: 1213, Price: 1 },
+  { Product: 'Tire', Store: 'American Tire', ProductID: 6167, Price: 50 },
+  { Product: 'Dog food', Store: 'Pet Food', ProductID: 6515, Price: 35 },
+  { Product: 'T-Shirt', Store: 'Best Clothing', ProductID: 4415, Price: 20 }
+];
+
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
-import {ProductListItemComponent} from '../product-list-item/product-list-item.component';
-import {OnlineStore} from '../Shared/store';
+import { NgForOf } from '@angular/common';
+import { ProductListItemComponent } from '../product-list-item/product-list-item.component';
+import { OnlineStore } from '../Shared/store';
+import { PRODUCTLIST } from '../data/mock-content'; // Import the array
+
 // New interface for products
 export interface Products {
   Product: string;
@@ -9,28 +11,20 @@ export interface Products {
   ProductID: number;
   Price: number;
 }
+
 @Component({
   selector: 'app-product-list',
-  imports: [
-    NgForOf,
-    ProductListItemComponent
-  ],
+  imports: [NgForOf, ProductListItemComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  // Product list
-  productList:Products[] =[
-    { Product: 'Apple', Store: 'Wallmart', ProductID: 1213, Price: 1 },
-    { Product: 'Tire', Store: 'American Tire', ProductID: 6167, Price: 50 },
-    { Product: 'Dog food', Store: 'Pet Food', ProductID: 6515, Price: 35 },
-    { Product: 'T-Shirt', Store: 'Best Clothing', ProductID: 4415, Price: 20 }
-  ];
+  // Use imported data
+  productList = PRODUCTLIST;
   //Catch the onclick event from the html
   selectedProduct?: Products;
   //function to set which product to display
   selectProduct(product: Products): void {
     this.selectedProduct = product;
   }
-
 }
