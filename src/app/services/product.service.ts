832c840366460fdddd48097aa69db8326c1f3bdb
Christiano <CN52@myscc.ca>
2025-10-09 23:26:55 -0400

    Updated ContentList and Added Read, Create, Update, and Delete methods to my service.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,7 +7,7 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private products = PRODUCTLIST;
+  private products :Products[] = PRODUCTLIST;
 
   constructor() {
 
@@ -16,6 +16,31 @@ export class ProductService {
   // Return all products
   getProducts(): Observable<Products[]> {
     return of(this.products);
+  }
+
+  // Add a new product
+  addProduct(newProduct: Products): Observable<Products[]> {
+    this.products.push(newProduct);
+    return of(this.products);
+  }
+
+  // Update an existing product
+  updateProduct(updatedProduct: Products): Observable<Products[]> {
+    const index = this.products.findIndex(p => p.ProductID === updatedProduct.ProductID);
+    if (index !== -1) {
+      this.products[index] = updatedProduct;
+    }
+    return of(this.products);
+  }
+
+  deleteProduct(productId: number): Observable<Products[]> {
+    this.products = this.products.filter(p => p.ProductID !== productId);
+    return of(this.products);
+  }
+  // Get a product by ID
+  getProductById(id: number): Observable<Products | undefined> {
+    const product = this.products.find(p => p.ProductID === id);
+    return of(product);
   }
 }
 
