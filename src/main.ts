e7437e90409c9bdc601b7ca262fa41994d14f4eb
Christiano <CN52@myscc.ca>
2025-10-17 00:42:10 -0400

    Double checked router is installed then ensured router is set up correctly then defined a list of routes in my main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {ProductListItemComponent} from './app/product-list-item/product-list-item.component';
+import {ProductListComponent} from "./app/product-list/product-list.component";
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path:'products', component: ProductListComponent},
+  {path:'products/:id', component: ProductListItemComponent}
+]
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
